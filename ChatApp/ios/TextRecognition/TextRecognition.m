17dfee93bc9e38735d0d21548e35e71629563147
Appinventiv <appinventiv@sam-macbook-pro.local>
2020-02-13 12:46:00 +0530

    done with text recognition

---
+++ ChatApp/ios/TextRecognition/TextRecognition.m
@@ -0,0 +1,9 @@
+//
+//  TextRecognition.m
+//  ChatApp
+//
+//  Created by Appinventiv on 12/02/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ChatApp/ios/TextRecognition/TextRecognition.swift
@@ -0,0 +1,9 @@
+//
+//  TextRecognition.swift
+//  ChatApp
+//
+//  Created by Appinventiv on 12/02/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
